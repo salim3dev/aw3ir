cca3d748f0035f3af59c60a87231feafd089b0fc
salim3dev <salim.ladj23@gmail.com>
2024-10-21 10:11:12 +0200

    Update form-validation.js

--- tp3/js/form-validation.js
+++ tp3/js/form-validation.js
@@ -32,20 +32,46 @@ window.onload = function () {
       if (!isValid) {
         var errorModal = new bootstrap.Modal(document.getElementById("myModal"));
         errorModal.show();
-      }
-  
+      } 
       // Si toutes les validations sont réussies, afficher la modal de validation
+      
+      
       else {
+        // Récupérer l'adresse depuis le formulaire
+        const address = document.querySelector("#address").value.trim();
+  
+        // Construire l'URL de Google Static Maps avec l'adresse saisie
+        const mapImageUrl = `https://maps.googleapis.com/maps/api/staticmap?markers=${encodeURIComponent(address)}&zoom=14&size=400x300&scale=2&key=AIzaSyAkmvI9DazzG9p77IShsz_Di7-5Qn7zkcg`;
+  
+        // Vérifier dans la console si l'URL est correcte
+        console.log(mapImageUrl);
+  
+        // Modifier le contenu de la modal pour afficher l'image et le lien vers Google Maps
+        document.querySelector("#resultModal .modal-body").innerHTML = `
+          <a href="http://maps.google.com/maps?q=${encodeURIComponent(address)}" target="_blank">
+            <img src="${mapImageUrl}" alt="Google Maps" style="width: 400; height: 300;">
+          </a>
+        `;
+  
+
+         // Vérification du contenu de la modal
+      console.log(document.querySelector("#resultModal .modal-body").innerHTML);
+
+
+
+        // Afficher la modal avec la carte
         var successModal = new bootstrap.Modal(document.getElementById("resultModal"));
         successModal.show();
       }
     });
   };
-  
+
+
   // Fonction de validation d'email
   function validateEmail(email) {
     const re =
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     return re.test(String(email).toLowerCase());
   }
+
   
