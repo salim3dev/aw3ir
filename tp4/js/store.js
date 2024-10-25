8d22642c3531134240eddf1be526de9c5a0ca562
salim3dev <salim.ladj23@gmail.com>
2024-10-25 09:37:00 +0200

    Update store.js

--- tp4/js/store.js
+++ tp4/js/store.js
@@ -1,41 +1,26 @@
-// store.js
-// Script pour gérer la liste de contact en JSON
-
-// Pour ajouter un contact : contactStore.add(_name, _firsname, _date, _adress, _mail);
-// Pour récupérer la liste : contactStore.getList();
-
 var contactStore = (function () {
-    // Charger la liste des contacts depuis localStorage ou initialiser une liste vide
     let contactListString = localStorage.getItem("contactList");
     var contactList = contactListString ? JSON.parse(contactListString) : [];
   
     return {
-      add: function (_name, _firsname, _date, _adress, _mail) {
-        var contact = {
-          name: _name,
-          firstname: _firsname,
-          date: _date,
-          adress: _adress,
-          mail: _mail,
-        };
-        // Ajouter le contact à la liste
+      add: function (_name, _firstname, _date, _address, _mail) {
+        var contact = { name: _name, firstname: _firstname, date: _date, address: _address, mail: _mail };
         contactList.push(contact);
-  
-        // Enregistrer la liste mise à jour dans localStorage
         localStorage.setItem("contactList", JSON.stringify(contactList));
-  
         return contactList;
+      },
+  
+      remove: function (index) {
+        contactList.splice(index, 1);
+        localStorage.setItem("contactList", JSON.stringify(contactList));
       },
   
       reset: function () {
-        // Vider la liste des contacts et localStorage
         contactList = [];
         localStorage.removeItem("contactList");
-        return contactList;
       },
   
       getList: function () {
-        // Renvoyer la liste des contacts
         return contactList;
       },
     };
